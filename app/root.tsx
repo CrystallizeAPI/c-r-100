ed0a5130d773f3532de10d0e80ff79490fba23eb
Håkon Krogh <hakonhk@gmail.com>
2022-03-28 12:19:21 +0200

    GTM

--- app/root.tsx
+++ app/root.tsx
@@ -28,8 +28,28 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MWTR635');
+        `,
+          }}
+        />
       </head>
       <body className="text-gray-900">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MWTR635"
+            height="0"
+            width="0"
+            title="GTM"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Layout>
           <Outlet />
         </Layout>
